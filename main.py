080f6cab789e19c237bcf95a839c2de36365c92a
old-blighty <bengreenberg@dyn1193-208.wlan.ic.ac.uk>
2016-06-17 14:35:52 +0100

    main.py update
    Updated to include more test functions.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ Authors:
     Grace Chin
     Fan Mo
 
-Main.py -- Version 1.3
+Main.py -- Version 1.4
 ''')
 
 def f(): # callback function run when switch is pressed
@@ -27,6 +27,34 @@ sw.callback(f) # callback event if USR switch is pressed
 
 choice = '' # declares 'choice' as initially empty
 isRepeat = False
+
+servo = Servo(3) # servo on position 3 (X3, VIN, GND)
+servo.angle(-45)
+
+bLED = Pin('X12', Pin.OUT_PP) # Blue LED // indicate recording
+yLED = Pin('X11', Pin.OUT_PP) # Yellow LED // currently unused
+rLED = Pin('X10', Pin.OUT_PP) # Red LED // indicate playback
+led1 = pyb.LED(1)
+led2 = pyb.LED(2)
+led3 = pyb.LED(3)
+led4 = pyb.LED(4)
+
+def toggleLED(led):
+    if led == 'r':
+        if rLED.value() == 0:
+            rLED.high()
+        elif rLED.value() == 1:
+            rLED.low()
+    elif led == 'y':
+        if yLED.value() == 0:
+            yLED.high()
+        elif yLED.value() == 1:
+            yLED.low()
+    elif led == 'b':
+        if bLED.value() == 0:
+            bLED.high()
+        elif bLED.value() == 1:
+            bLED.low()
 
 def start():
     global choice
@@ -46,11 +74,15 @@ def start():
 
     if choice == 'help':
         print('- Help Menu -\nHere are the options available to you:')
-        print('autodrive')
-        print('task5')
-        print('pilot')
-        print('led')
-        print('servo')
+        print('''autodrive
+        task5
+        pilot
+        led
+        servo
+        leds
+        boardleds
+        ''')
+        start()
 
     elif choice == 'autodrive':
         execfile('autodrive.py')
@@ -66,10 +98,39 @@ def start():
         bLED.high()
 
     elif choice == 'servo':
-        s1 = Servo(3) # servo on position 3 (X3, VIN, GND)
+        servo = Servo(3) # servo on position 3 (X3, VIN, GND)
         while True:
             angle = int(input('Angle: '))
-            s1.angle(angle) # move to user defined angle
+            servo.angle(angle) # move to user defined angle
+
+    elif choice == 'leds':
+        bLED = Pin('X12', Pin.OUT_PP)
+        yLED = Pin('X11', Pin.OUT_PP)
+        rLED = Pin('X10', Pin.OUT_PP)
+        bLED.high()
+        yLED.high()
+        rLED.high()
+
+    elif choice == 'boardleds':
+        led1 = pyb.LED(1)
+        led2 = pyb.LED(2)
+        led3 = pyb.LED(3)
+        led4 = pyb.LED(4)
+        while True:
+            num = int(input('Choose LED number:'))
+            if num == 1:
+                led1.toggle()
+            elif num == 2:
+                led2.toggle()
+            elif num == 3:
+                led3.toggle()
+            elif num == 4:
+                led4.toggle()
+            elif num == 0:
+                led1.off()
+                led2.off()
+                led3.off()
+                led4.off()
 
     else:
         print('You did not type a valid mode name. Please try again, or try typing help')
